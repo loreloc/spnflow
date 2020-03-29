fixed wrong likelihood and log likelihood calculation

src/spnflow/algorithms/inference.py
@@ -33,22 +33,32 @@ def eval_bottom_up(root, x, leaf_func, node_func, return_results=False):
 
 
 def leaf_likelihood(node, x, m):
-    y = np.ones(shape=(x.shape[0], 1))
-    y[~m] = node.likelihood(x[~m])
-    return y
+    z = np.ones(shape=(x.shape[0], 1))
+    z[~m] = node.likelihood(x[~m])
+    z[np.isnan(z)] = 1.0
+    z[np.isinf(z)] = np.finfo(float).eps
+    return z
 
 
 def leaf_log_likelihood(node, x, m):
-    y = np.zeros(shape=(x.shape[0], 1))
-    y[~m] = node.log_likelihood(x[~m])
-    return y
+    z = np.zeros(shape=(x.shape[0], 1))
+    z[~m] = node.log_likelihood(x[~m])
+    z[np.isnan(z)] = 0.0
+    z[np.isinf(z)] = np.finfo(float).min
+    return z
 
 
 def node_likelihood(node, lc):
-    z = np.concatenate(lc, axis=1)
-    return node.likelihood(z)
+    x = np.concatenate(lc, axis=1)
+    z = node.likelihood(x)
+    z[np.isnan(z)] = 1.0
+    z[np.isinf(z)] = np.finfo(float).eps
+    return z
 
 
 def node_log_likelihood(node, lc):
-    z = np.concatenate(lc, axis=1)
-    return node.log_likelihood(z)
+    x = np.concatenate(lc, axis=1)
+    z = node.log_likelihood(x)
+    z[np.isnan(z)] = 0.0
+    z[np.isinf(z)] = np.finfo(float).min
+    return z

src/spnflow/examples/mnist/mnist.py
@@ -43,7 +43,7 @@ if __name__ == '__main__':
 
     # Learn the SPN classifier
     distributions = [Gaussian] * n_features + [Multinomial]
-    spn = learn_classifier(train_data, distributions, class_idx=n_features, n_jobs=4, min_rows_slice=2048)
+    spn = learn_classifier(train_data, distributions, class_idx=n_features, n_jobs=4, min_rows_slice=1024)
     assert_is_valid(spn)
     print("SPN Statistics: " + str(get_statistics(spn)))
     spn = prune(spn)
@@ -51,6 +51,8 @@ if __name__ == '__main__':
     print("SPN Statistics after pruning: " + str(get_statistics(spn)))
 
     # Evaluate some metrics
+    l = likelihood(spn, test_data)
+    print("Likelihood: " + str(np.mean(l)))
     ll = log_likelihood(spn, test_data)
     print("Log Likelihood: " + str(np.mean(ll)))
     test_data[:, n_features] = np.nan

src/spnflow/structure/node.py
@@ -27,9 +27,7 @@ class Sum(Node):
         return np.dot(x, self.weights).reshape(-1, 1)
 
     def log_likelihood(self, x):
-        z = np.copy(x)
-        z[np.isinf(z)] = np.finfo(float).min
-        return logsumexp(z, b=self.weights, axis=1).reshape(-1, 1)
+        return logsumexp(x, b=self.weights, axis=1).reshape(-1, 1)
 
 
 class Mul(Node):
@@ -42,9 +40,7 @@ class Mul(Node):
         return np.prod(x, axis=1).reshape(-1, 1)
 
     def log_likelihood(self, x):
-        z = np.copy(x)
-        z[np.isinf(z)] = np.finfo(float).min
-        return np.sum(z, axis=1).reshape(-1, 1)
+        return np.sum(x, axis=1).reshape(-1, 1)
 
 
 def assign_ids(root):