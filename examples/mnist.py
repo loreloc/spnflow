fixed log likelihood calculation

examples/mnist.py
@@ -1,7 +1,6 @@
 import numpy as np
 import tensorflow as tf
 from spnflow.model import build_spn
-from spnflow.distributions import NormalLayer
 import matplotlib.pyplot as plt
 
 
@@ -18,7 +17,7 @@ def plot_fit_history(history, metric='loss', title='Untitled'):
 def get_loss_function(kind='mixed', lam=1.0):
     @tf.function
     def log_loss(y_true, y_pred):
-        return tf.math.negative(tf.reduce_mean(y_pred))
+        return tf.math.negative(y_pred)
 
     @tf.function
     def cross_entropy(y_true, y_pred):
@@ -54,9 +53,8 @@ if __name__ == '__main__':
     depth = 2
     n_sum = 10
     n_dists = 2
-    n_repetitions = 10
-    dist_class = NormalLayer
-    spn = build_spn(n_features, n_classes, depth, dist_class, n_sum, n_dists, n_repetitions)
+    n_reps = 20
+    spn = build_spn(n_features, n_classes, depth, n_sum, n_dists, n_reps)
 
     # Print some summary
     spn.summary()
@@ -66,7 +64,7 @@ if __name__ == '__main__':
     spn.compile(optimizer='adam', loss=loss_fn, metrics=['accuracy'])
 
     # Fit the model
-    history = spn.fit(x_train, y_train, batch_size=256, epochs=100, validation_data=(x_test, y_test))
+    history = spn.fit(x_train, y_train, batch_size=256, epochs=50, validation_data=(x_test, y_test))
 
     # Plot the train history
     plot_fit_history(history, metric='loss', title='Loss')

spnflow/layers.py
@@ -1,24 +1,26 @@
+import numpy as np
 import tensorflow as tf
+import tensorflow_probability as tfp
 
 
-class DistributionsLayer(tf.keras.layers.Layer):
+class GaussianLayer(tf.keras.layers.Layer):
     """
-    The distributions input layer class.
+    The Gaussian distributions input layer class.
     """
-    def __init__(self, regions, dist_class, n_dists, **kwargs):
+    def __init__(self, regions, n_dists, **kwargs):
         """
-        Initialize a distributions input layer.
+        Initialize a Gaussian distributions input layer.
 
         :param regions: The regions of the distributions.
-        :param dist_class: The leaves distributions class.
         :param n_dists: The number of distributions.
         :param kwargs: Other arguments.
         """
-        super(DistributionsLayer, self).__init__(**kwargs)
+        super(GaussianLayer, self).__init__(**kwargs)
         self.regions = regions
-        self.dist_class = dist_class
         self.n_dists = n_dists
-        self._layers = []
+        self._means = None
+        self._scales = None
+        self._distributions = None
 
     def build(self, input_shape):
         """
@@ -26,12 +28,32 @@ class DistributionsLayer(tf.keras.layers.Layer):
 
         :param input_shape: The input shape.
         """
-        # Add the gaussian distribution leaves
-        for region in self.regions:
-            self._layers.append(self.dist_class(region, self.n_dists))
+        # Create the means variables
+        self._means = [
+            tf.Variable(
+                tf.random.normal(shape=(self.n_dists, len(r)), stddev=1e-1),
+                trainable=True
+            )
+            for r in self.regions
+        ]
+
+        # Create the scales variables
+        self._scales = [
+            tf.Variable(
+                1.0 + 1e-1 * tf.math.sigmoid(tf.random.normal(shape=(self.n_dists, len(r)))),
+                trainable=True
+            )
+            for r in self.regions
+        ]
+
+        # Create the multi-batch multivariate distributions
+        self._distributions = [
+            tfp.distributions.MultivariateNormalDiag(mean, scale)
+            for mean, scale in zip(self._means, self._scales)
+        ]
 
         # Call the parent class's build method
-        super(DistributionsLayer, self).build(input_shape)
+        super(GaussianLayer, self).build(input_shape)
 
     @tf.function
     def call(self, inputs):
@@ -41,7 +63,13 @@ class DistributionsLayer(tf.keras.layers.Layer):
         :param inputs: The inputs.
         :return: The log likelihood of each distribution leaf.
         """
-        return tf.stack([d(inputs) for d in self._layers], axis=1)
+        # Concatenate the results of each distribution result
+        ll = [
+            d.log_prob(tf.expand_dims(tf.gather(inputs, r, axis=1), axis=1))
+            for r, d in zip(self.regions, self._distributions)
+        ]
+        x = tf.stack(ll, axis=1)
+        return x
 
 
 class ProductLayer(tf.keras.layers.Layer):
@@ -52,10 +80,11 @@ class ProductLayer(tf.keras.layers.Layer):
         """
         Initialize the Product layer.
 
+        :param partitions: The partitions list.
         :param kwargs: Parent class arguments.
         """
         super(ProductLayer, self).__init__(**kwargs)
-        self.n_regions = None
+        self.n_partitions = None
         self.n_nodes = None
 
     def build(self, input_shape):
@@ -64,9 +93,9 @@ class ProductLayer(tf.keras.layers.Layer):
 
         :param input_shape: The input shape.
         """
-        # Set the number of child regions and the number of product nodes per partition
-        self.n_regions = input_shape[1]
-        self.n_nodes = input_shape[2] ** 2
+        # Set the number of partitions and the number of nodes for each region channel
+        self.n_partitions = input_shape[1] // 2
+        self.n_nodes = input_shape[2]
 
         # Call the parent class build method
         super(ProductLayer, self).build(input_shape)
@@ -79,27 +108,26 @@ class ProductLayer(tf.keras.layers.Layer):
         :param inputs: The inputs.
         :return: The tensor result of the layer.
         """
-        dist0 = tf.gather(inputs, [i for i in range(self.n_regions) if i % 2 == 0], axis=1)
-        dist1 = tf.gather(inputs, [i for i in range(self.n_regions) if i % 2 == 1], axis=1)
-        result = tf.expand_dims(dist0, 2) + tf.expand_dims(dist1, 3)
-        return tf.reshape(result, [-1, self.n_regions // 2, self.n_nodes])
+        # Compute the outer product (the "outer sum" in log domain)
+        x = tf.reshape(inputs, [-1, 2, self.n_partitions, self.n_nodes])
+        x = tf.expand_dims(x[:, 0], 3) + tf.expand_dims(x[:, 1], 2)
+        x = tf.reshape(x, [-1, self.n_partitions, self.n_nodes ** 2])
+        return x
 
 
 class SumLayer(tf.keras.layers.Layer):
     """
     Sum node layer.
     """
-    def __init__(self, n_sum, is_root=False, **kwargs):
+    def __init__(self, n_sum, **kwargs):
         """
         Initialize the sum layer.
 
         :param n_sum: The number of sum node per region.
-        :param is_root: A boolean indicating if the sum layer is a root layer.
         :param kwargs: Parent class arguments.
         """
         super(SumLayer, self).__init__(**kwargs)
         self.n_sum = n_sum
-        self.is_root = is_root
         self.kernel = None
 
     def build(self, input_shape):
@@ -109,15 +137,11 @@ class SumLayer(tf.keras.layers.Layer):
         :param input_shape: The input shape.
         """
         # Set the kernel shape
-        kernel_shape = None
-        if self.is_root:
-            kernel_shape = (1, input_shape[1], self.n_sum)
-        else:
-            kernel_shape = (input_shape[1], input_shape[2], self.n_sum)
+        kernel_shape = (input_shape[1], input_shape[2], self.n_sum)
 
         # Construct the weights
         self.kernel = tf.Variable(
-            initial_value=tf.random.normal(kernel_shape, stddev=5e-1),
+            initial_value=tf.random.normal(kernel_shape, stddev=1e-1),
             trainable=True
         )
 
@@ -132,8 +156,8 @@ class SumLayer(tf.keras.layers.Layer):
         :param inputs: The inputs.
         :return: The tensor result of the layer.
         """
-        # Calculate the log likelihood using the logsumexp trick
+        # Calculate the log likelihood using the "logsumexp" trick
         x = tf.expand_dims(inputs, axis=-1)
-        x = x + tf.math.log_softmax(self.kernel, axis=2)
-        x = tf.math.reduce_logsumexp(x, axis=-2)
+        w = tf.math.log_softmax(self.kernel, axis=2)
+        x = tf.math.reduce_logsumexp(x + w, axis=2)
         return x

spnflow/model.py
@@ -1,26 +1,22 @@
 import tensorflow as tf
 from spnflow.region import RegionGraph
-from spnflow.layers import DistributionsLayer, ProductLayer, SumLayer
+from spnflow.layers import GaussianLayer, ProductLayer, SumLayer
 
 
-def build_spn(n_features, n_classes, depth, dist_class, n_sum=2, n_dists=2, n_reps=1, seed=42):
+def build_spn(n_features, n_classes, depth, n_sum=2, n_dists=2, n_reps=1, seed=42):
     """
     Build a RAT-SPN model.
 
     :param n_features: The number of features.
     :param n_classes: The number of classes.
     :param depth: The depth of the network.
-    :param dist_class: The base distributions class.
     :param n_sum: The number of sum nodes.
     :param n_dists: The number of distributions.
     :param n_reps: The number of independent repetitions of the region graph.
     :return: A Keras based RAT-SPN model.
     """
-    # Instantiate the features list
-    features = list(range(n_features))
-
     # Instantiate the region graph
-    region_graph = RegionGraph(features, seed=seed)
+    region_graph = RegionGraph(n_features, depth=depth, seed=seed)
 
     # Create the input layer
     input_layer = tf.keras.layers.Input(shape=(n_features,))
@@ -28,13 +24,13 @@ def build_spn(n_features, n_classes, depth, dist_class, n_sum=2, n_dists=2, n_re
     # Hidden layers build loop
     hidden_layers = []
     for _ in range(n_reps):
-        region_graph.clear()
-        region_graph.random_split(depth)
-        region_graph.make_layers()
+        # Compute a random graph layers
+        layers = list(reversed(region_graph.random_layers()))
 
         # Build the input distributions layer
-        layers = region_graph.layers()
-        x = DistributionsLayer(layers[0], dist_class, n_dists)(input_layer)
+        x = GaussianLayer(layers[0], n_dists)(input_layer)
+
+        # Alternate between product and sum layer
         for i in range(1, len(layers) - 1):
             if i % 2 == 1:
                 x = ProductLayer()(x)
@@ -45,13 +41,13 @@ def build_spn(n_features, n_classes, depth, dist_class, n_sum=2, n_dists=2, n_re
     # Build the concatenation layer
     concat_layer = tf.keras.layers.Concatenate()(hidden_layers)
 
-    # Build the flatten layer
-    flatten_layer = tf.keras.layers.Flatten()(concat_layer)
-
     # Build the root layer
-    root_layer = SumLayer(n_classes, is_root=True)(flatten_layer)
+    root_layer = SumLayer(n_classes)(concat_layer)
+
+    # Build the flatten layer
+    output_layer = tf.keras.layers.Flatten()(root_layer)
 
     # Build the model
-    model = tf.keras.Model(inputs=input_layer, outputs=root_layer)
+    model = tf.keras.Model(inputs=input_layer, outputs=output_layer)
 
     return model

spnflow/region.py
@@ -27,160 +27,48 @@ class RegionGraph:
 
     In the end, this procedure will always deliver a complete and decomposable SPN.
     """
-    def __init__(self, items, seed=42):
+    def __init__(self, n_features, depth, seed=42):
         """
         Initialize a region graph.
 
-        :param items: The items of the root region.
+        :param n_features: The number of features.
+        :param depth: The maximum depth.
         :param seed: The random state's seed.
         """
-        self._items = tuple(sorted(items))
+        self._items = tuple(range(n_features))
+        self._depth = depth
         self._rnd = np.random.RandomState(seed)
-        self._regions = set()
-        self._children = dict()
-        self._partitions = set()
-        self._layers = list()
 
-    def items(self):
+    def random_layers(self):
         """
-        Get the items of the region graph.
+        Generate a region graph randomly.
 
-        :return: The items.
+        :return: A list of layers, alternating between regions and partitions.
         """
-        return self._items
+        root = [self._items]
+        layers = [root]
 
-    def regions(self):
-        """
-        Get the regions of the region graph.
+        for i in range(self._depth):
+            regions = []
+            partitions = []
+            for r in layers[i * 2]:
+                mid = len(r) // 2
+                if mid == 0:
+                    return layers
+                permutation = self._rnd.permutation(r).tolist()
+                p0 = tuple(permutation[:mid])
+                p1 = tuple(permutation[mid:])
+                regions.append(p0)
+                regions.append(p1)
+                partitions.append((p0, p1))
+            layers.append(partitions)
+            layers.append(regions)
 
-        :return: The regions.
-        """
-        return self._regions
-
-    def children(self):
-        """
-        Get the children dictionary of the region graph.
-
-        :return: The children dictionary.
-        """
-        return self._children
-
-    def partitions(self):
-        """
-        Get the partitions of the region graph.
-
-        :return: The partitions.
-        """
-        return self._partitions
-
-    def layers(self):
-        """
-        Get the layers of the region graph. The returned value is an empty list if *make_layers* is not called.
-
-        :return: The layers.
-        """
-        return self._layers
-
-    def leaves(self):
-        """
-        Get the leaves of the region graph.
-
-        :return: The leaves.
-        """
-        return [x for x in self._regions if x not in self._children]
-
-    def clear(self):
-        """
-        Clear The partitions and layers.
-        """
-        self._regions.clear()
-        self._children.clear()
-        self._partitions.clear()
-        self._layers.clear()
-
-    def random_split(self, depth):
-        """
-        Split the ragion graph randomly given a certain depth.
-
-        :param depth: The maximum depth.
-        """
-        assert depth > 0, "The depth must be greater than zero"
-
-        # Start with the root region
-        tasks = list()
-        tasks.append((self._items, depth))
-
-        while tasks:
-            # Permute and split in half the region
-            (items, depth) = tasks.pop()
-            items = self._rnd.permutation(items).tolist()
-            mid = len(items) // 2
-            r1 = tuple(sorted(items[:mid]))
-            r2 = tuple(sorted(items[mid:]))
-
-            # Create a partition from the region split
-            partition = tuple(sorted((r1, r2)))
-            self._regions.add(r1)
-            self._regions.add(r2)
-
-            # Update the partitions list and the children dictionary
-            parent = tuple(sorted(x for child in partition for x in child))
-            if partition not in self._partitions:
-                self._partitions.add(partition)
-                children = self._children.get(parent, [])
-                self._children[parent] = children + [partition]
-
-            # Recursively run the random split over the sub-regions
-            if depth > 1:
-                if len(r1) > 1:
-                    tasks.append((r1, depth - 1))
-                if len(r2) > 1:
-                    tasks.append((r2, depth - 1))
-
-    def make_layers(self):
-        """
-        Make a layered structure, represented as a list of lists.
-
-        The layered representation is greedily constructed, in order to contain as few layers as possible.
-        Crucially it repstects some topological order of the region graph (a directed graph is acyclic if and only if
-        there exists a topological order of its node), i.e. if k >= 1, it is guaranteed that regions (or partitions) in
-        layer k cannot be children of partitions (or regions) in layer l.
-
-        _layers[0] will contain leaf regions.
-        For k > 0:
-            _layers[k] will contain partitions, if k is odd
-            _layers[k] will contain regions, if k is even
-        """
-        seen_regions = set()
-        seen_partitions = set()
-
-        # Start with the first layer composed by the leaves of the region graph
-        leaves = sorted(self.leaves())
-        self._layers = [leaves]
-        seen_regions.update(leaves)
-
-        # While there are regions or partitions to process ...
-        while len(seen_regions) != len(self._regions) or len(seen_partitions) != len(self.partitions()):
-            # The next partition layer contains all partitions which have not been visited and
-            # all its child regions have been visited
-            next_layer = [p for p in self._partitions
-                          if p not in seen_partitions and all([r in seen_regions for r in p])]
-            self._layers.append(next_layer)
-            seen_partitions.update(next_layer)
-
-            # The next region layer contains all regions which have not been visited and
-            # all its child partitions have been visited.
-            next_layer = [r for r in self._regions
-                          if r not in seen_regions and all([p in seen_partitions for p in self._children[r]])]
-            next_layer = sorted(next_layer)
-            self._layers.append(next_layer)
-            seen_regions.update(next_layer)
+        return layers
 
 
 if __name__ == '__main__':
-    rg = RegionGraph([0, 1, 2, 3, 4, 5, 6, 7, 8])
-    rg.random_split(3)
-    rg.make_layers()
-    layers = rg.layers()
-    for layer in reversed(layers):
+    rg = RegionGraph([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], depth=4)
+    layers = rg.random_layers()
+    for layer in layers:
         print(layer)