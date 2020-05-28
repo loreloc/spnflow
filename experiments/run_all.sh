46d72376969aa1c1eb3c975b50231b73ab6d2089
loreloc <lorenzoloconte@outlook.it>
2020-05-28 21:14:51 +0200

    disabled GPU use for power and gas datasets

--- experiments/run_all.sh
+++ experiments/run_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export PYTHONPATH=../
-python experiment.py power
-python experiment.py gas
+CUDA_VISIBLE_DEVICES="" python experiment.py power
+CUDA_VISIBLE_DEVICES="" python experiment.py gas
 python experiment.py hepmass
 python experiment.py miniboone
 python experiment.py bsds300
