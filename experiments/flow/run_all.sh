2ba5adbd38b22e17d21af404e0d04defd28e2458
loreloc <lorenzoloconte@outlook.it>
2020-06-09 09:10:08 +0200

    reordered experiments execution in run_all.sh

--- experiments/flow/run_all.sh
+++ experiments/flow/run_all.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 export PYTHONPATH=../../
 export DATAPATH=../
+python experiment.py miniboone
+python experiment.py hepmass
 python experiment.py power
 python experiment.py gas
-python experiment.py hepmass
-python experiment.py miniboone
 python experiment.py bsds300
 python experiment.py mnist
 python experiment.py cifar10
