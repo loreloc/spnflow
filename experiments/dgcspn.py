fix dgcspn bits per dimension computation

experiments/dgcspn.py
@@ -50,7 +50,7 @@ if __name__ == '__main__':
     rand_state = np.random.RandomState(42)
 
     assert args.quantiles_loc is False or args.uniform_loc is None, \
-        'Only one between --mean-quantiles and --mean-uniform can be defined'
+        'Only one between --quantiles-loc and --uniform-loc can be defined'
 
     # Load the dataset
     transform = DataStandardizer(sample_wise=False, flatten=False)

experiments/utils.py
@@ -28,7 +28,7 @@ def collect_results_generative(model, data_train, data_valid, data_test, compute
     # Compute the bits per pixel, if specified
     if compute_bpp:
         dims = np.prod(data_train.shape[1:])
-        bpp = np.log2(256) - (mu_ll / (dims * np.log(2)))
+        bpp = -(mu_ll / np.log(2)) / dims
         return mu_ll, sigma_ll, bpp
     else:
         return mu_ll, sigma_ll, None