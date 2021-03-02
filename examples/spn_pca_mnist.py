26bcb127384bf794087199266744a8af1a6e3988
loreloc <lorenzoloconte@outlook.it>
2021-03-02 18:40:19 +0100

    fixed mnist spn example

--- examples/spn_pca_mnist.py
+++ examples/spn_pca_mnist.py
@@ -19,29 +19,33 @@ data_train = torchvision.datasets.MNIST('datasets', train=True, transform=None, 
 data_test = torchvision.datasets.MNIST('datasets', train=False, transform=None, download=True)
 x_train, y_train = data_train.data.numpy(), data_train.targets.numpy()
 x_test, y_test = data_test.data.numpy(), data_test.targets.numpy()
-
-# Preprocess the dataset
-transform = DataStandardizer(sample_wise=False, flatten=True)
-transform.fit(x_train)
-x_train = transform.forward(x_train)
-x_test = transform.forward(x_test)
+x_train = np.reshape(x_train, [len(x_train), -1])
+x_test = np.reshape(x_test, [len(x_test), -1])
 
 # Apply PCA for dimensionality reduction
-n_components = 40
+n_components = 32
 pca = sk.decomposition.PCA(n_components)
 pca.fit(x_train)
 x_train = pca.transform(x_train)
 x_test = pca.transform(x_test)
 
+# Preprocess the dataset
+transform = DataStandardizer()
+transform.fit(x_train)
+x_train = transform.forward(x_train)
+x_test = transform.forward(x_test)
+
 # Learn the SPN structure and parameters, as a classifier
 distributions = [Gaussian] * n_components + [Multinomial]
 data_train = np.column_stack([x_train, y_train])
 spn = learn_classifier(
-    data_train, distributions,
+    data_train,
+    distributions,
     learn_leaf='mle',
     split_rows='kmeans',
     split_cols='gvs',
-    min_rows_slice=128,
+    min_rows_slice=200,
+    split_rows_kwargs={'n': 2},
     split_cols_kwargs={'p': 1.0}
 )
 
@@ -67,7 +71,7 @@ data_samples = np.column_stack([nans, classes])
 data_samples = sample(spn, data_samples)[:, :-1]
 
 # Transform back the data and plot the samples
-data_images = transform.backward(pca.inverse_transform(data_samples))
-data_images = data_images.reshape(n_samples * n_classes, 1, image_w, image_h)
+data_images = pca.inverse_transform(transform.backward(data_samples))
+data_images = data_images.reshape(n_samples * n_classes, 1, image_h, image_w)
 data_images = data_images / 255.0
 torchvision.utils.save_image(torch.tensor(data_images), 'samples_spn_mnist.png', nrow=n_samples, padding=0)
