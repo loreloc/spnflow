7053faced134cce214fdd2b552484fc92b58737f
loreloc <lorenzoloconte@outlook.it>
2021-03-03 21:06:49 +0100

    centered moons data in nvp1d_moons example

--- examples/nvp1d_moons.py
+++ examples/nvp1d_moons.py
@@ -8,17 +8,18 @@ from spnflow.torch.routines import torch_train
 
 
 x_train, y_train = make_moons(n_samples=10000, shuffle=True, noise=0.05)
+x_train = x_train - [0.5, 0.25]
 x_train = x_train.astype(np.float32)
 x_train, x_valid, y_train, y_valid = train_test_split(x_train, y_train, test_size=0.2)
 
 plt.scatter(x_train[:, 0], x_train[:, 1], marker='o', s=2)
 plt.show()
 
-model = RealNVP1d(in_features=2, n_flows=5, depth=2, units=128, batch_norm=False)
+model = RealNVP1d(in_features=2, n_flows=10, depth=2, units=128, batch_norm=False)
 torch_train(
     model, x_train, x_valid,
     setting='generative', lr=1e-4,
-    batch_size=100, epochs=100, patience=20
+    batch_size=100, epochs=200, patience=10
 )
 
 samples = model.sample(2000).cpu().numpy()
