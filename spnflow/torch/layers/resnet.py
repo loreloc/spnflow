b305c5d674db27a939c82ca970efc9173a6e7078
loreloc <lorenzoloconte@outlook.it>
2021-05-06 10:31:25 +0200

    introduced skip connections in resnet implementation

--- spnflow/torch/layers/resnet.py
+++ spnflow/torch/layers/resnet.py
@@ -17,6 +17,8 @@ class ResidualBlock(torch.nn.Module):
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.block = torch.nn.Sequential(
+            torch.nn.BatchNorm2d(self.out_channels),
+            torch.nn.ReLU(),
             WeightNormConv2d(
                 self.in_channels, self.out_channels,
                 kernel_size=kernel_size, padding=padding, bias=False
@@ -25,9 +27,8 @@ class ResidualBlock(torch.nn.Module):
             torch.nn.ReLU(),
             WeightNormConv2d(
                 self.out_channels, self.out_channels,
-                kernel_size=kernel_size, padding=padding, bias=False
-            ),
-            torch.nn.BatchNorm2d(self.out_channels)
+                kernel_size=kernel_size, padding=padding, bias=True
+            )
         )
 
     def forward(self, x):
@@ -37,11 +38,11 @@ class ResidualBlock(torch.nn.Module):
         :param x: The inputs.
         :return: The outputs of the block.
         """
-        return torch.relu(x + self.block(x))
+        return x + self.block(x)
 
 
 class ResidualNetwork(torch.nn.Module):
-    """Residual network (aka ResNet)."""
+    """Residual network (aka ResNet) with skip connections."""
     def __init__(self, in_channels, mid_channels, out_channels, n_blocks):
         """
         Initialize a residual network.
@@ -56,32 +57,35 @@ class ResidualNetwork(torch.nn.Module):
         self.mid_channels = mid_channels
         self.out_channels = out_channels
         self.n_blocks = n_blocks
-        self.layers = torch.nn.ModuleList()
+        self.blocks = torch.nn.ModuleList()
+        self.skips = torch.nn.ModuleList()
 
-        # Build the input layers
-        self.layers.extend([
-            torch.nn.Conv2d(
-                self.in_channels, self.mid_channels,
-                kernel_size=(3, 3), padding=(1, 1), bias=False),
-            torch.nn.BatchNorm2d(self.mid_channels),
-            torch.nn.ReLU()
-        ])
+        # Build the input convolutional layer and the first skip layer
+        self.in_conv = WeightNormConv2d(
+            self.in_channels, self.mid_channels,
+            kernel_size=(3, 3), padding=(1, 1), bias=True
+        )
+        self.in_skip = WeightNormConv2d(
+            self.mid_channels, self.mid_channels,
+            kernel_size=(1, 1), padding=(0, 0), bias=True
+        )
 
-        # Build the inner residual blocks
+        # Build the inner residual blocks and the inner skip layers
         for _ in range(self.n_blocks):
-            self.layers.append(
-                ResidualBlock(
-                    self.mid_channels, self.mid_channels,
-                    kernel_size=(3, 3), padding=(1, 1)
-                )
-            )
-
-        # Build the output layer
-        self.layers.append(
-            torch.nn.Conv2d(
-                self.mid_channels, self.out_channels,
+            self.blocks.append(ResidualBlock(
+                self.mid_channels, self.mid_channels,
+                kernel_size=(3, 3), padding=(1, 1)
+            ))
+            self.skips.append(WeightNormConv2d(
+                self.mid_channels, self.mid_channels,
                 kernel_size=(1, 1), padding=(0, 0), bias=True
-            )
+            ))
+
+        # Build the output batch normalization and output convolutional layers
+        self.out_norm = torch.nn.BatchNorm2d(self.mid_channels)
+        self.out_conv = WeightNormConv2d(
+            self.mid_channels, self.out_channels,
+            kernel_size=(1, 1), padding=(0, 0), bias=True
         )
 
     def forward(self, x):
@@ -91,6 +95,15 @@ class ResidualNetwork(torch.nn.Module):
         :param x: The inputs.
         :return: The outputs of the module.
         """
-        for layer in self.layers:
-            x = layer(x)
-        return x
+        # Pass through the input layers
+        x = self.in_conv(x)
+        z = self.in_skip(x)
+
+        # Apply the inner layers
+        for block, skip in zip(self.blocks, self.skips):
+            x = block(x)
+            z += skip(x)
+
+        # Pass through the output layers
+        x = torch.relu(self.out_norm(z))
+        return self.out_conv(x)
