fix bits-per-pixels calculation

experiments/datasets.py
@@ -52,9 +52,9 @@ class UnsupervisedDataset(torch.utils.data.Dataset):
 
         # Compute the features shape
         if self.transform is None:
-            self.shape = self.data.shape[1:]
+            self.shape = tuple(self.data.shape[1:])
         else:
-            self.shape = self.transform(self.data[0]).shape
+            self.shape = tuple(self.transform(self.data[0]).shape)
 
     def features_size(self):
         if len(self.shape) == 1:
@@ -83,9 +83,9 @@ class SupervisedDataset(torch.utils.data.Dataset):
 
         # Compute the features shape
         if self.transform is None:
-            self.shape = self.data.shape[1:]
+            self.shape = tuple(self.data.shape[1:])
         else:
-            self.shape = self.transform(self.data[0]).shape
+            self.shape = tuple(self.transform(self.data[0]).shape)
 
     def num_classes(self):
         return len(self.classes)

experiments/utils.py
@@ -27,7 +27,7 @@ def collect_results_generative(model, data_train, data_valid, data_test, compute
 
     # Compute the bits per pixel, if specified
     if compute_bpp:
-        dims = np.prod(data_train.shape[1:])
+        dims = np.prod(data_train.features_size())
         bpp = -(mu_ll / np.log(2)) / dims
         return mu_ll, sigma_ll, bpp
     else: